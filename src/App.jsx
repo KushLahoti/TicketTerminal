33c1de110a25df693c60402a0c713a3da100f1c7
KushLahoti <kushlahoti123@gmail.com>
2025-06-18 16:17:26 +0530

    feat: initialize React project with Vite, Tailwind CSS, and essential components
    - Added package.json with dependencies and scripts for development
    - Included favicon and Vite logo in the public directory
    - Created main application structure with App component and routing
    - Developed Navbar and Footer components for navigation and layout
    - Implemented pages: Home, Movies, MovieDetails, SeatLayout, MyBookings, and Favourite
    - Set up global styles with Tailwind CSS and custom CSS variables
    - Configured Vite for React and Tailwind CSS integration

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import Footer from './components/Footer'
+import { Toaster } from 'react-hot-toast'
+
+const App = () => {
+
+  const isAdmin = useLocation().pathname.startsWith('/admin');
+
+  return (
+    <>
+      <Toaster />
+      {!isAdmin && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/myBookings' element={<MyBookings />} />
+        <Route path='/favourites' element={<Favourite />} />
+      </Routes>
+      {!isAdmin && <Footer />}
+    </>
+  )
+}
+
+export default App
